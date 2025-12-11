4a7b9f8fbbdd9b7d58a96d85f09b01b8ecae1ee2
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-11 20:28:32 +0600

    using if-Else statement

---
+++ bin/ifElseStatement.dart
@@ -0,0 +1,14 @@
+void main(){
+ ifElseExaple();
+}
+void ifElseExaple(){
+  int temperature=30;
+
+  if(temperature>30){
+    print('It is hot day');
+  }else if(temperature>20){
+    print('It is a pleasent day');
+  }else{
+    print('it\'s a cold day');
+  }
+}
