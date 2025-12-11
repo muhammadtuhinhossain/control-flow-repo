80b7cd2bef8cb92755eb9189d72e12fc646e3aa7
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-11 20:53:40 +0600

    using Switch Case statement

---
+++ bin/switch-case_statement.dart
@@ -0,0 +1,17 @@
+void main(){
+ switchCaseStatement();
+}
+void switchCaseStatement(){
+  String day='Monday';
+
+  switch (day){
+    case 'Monday':
+      print('Start of the work week');
+    break;
+    case 'Friday':
+      print('End of the work week');
+      break;
+      default:
+        print('Midweek day');
+  }
+}
