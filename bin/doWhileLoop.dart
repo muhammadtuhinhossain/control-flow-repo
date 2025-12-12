963c7fe367de85516a27969e08cc3024281e398b
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-12 21:03:46 +0600

    Run at least once condition is true or false

---
+++ bin/doWhileLoop.dart
@@ -0,0 +1,18 @@
+void main(){
+  int number=1;
+
+  do{
+    print('This will print even though number > 5');
+    number++;
+  }while(number<=3);
+
+  int num=4;
+
+  // A do-while loop will execute at least once,
+  // the condition is true or false.
+
+  do{
+    print('This will print even though number > 10');
+    number++;
+  }while(num<=3);
+}
