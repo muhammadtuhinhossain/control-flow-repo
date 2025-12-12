429c8e1966d76472fdfb0574684533e53d512f0c
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-12 23:47:00 +0600

    Add terminal input using switch check number is even or odd

---
+++ bin/switchCaseEvenOdd.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main(){
+  print('Enter your choice ');
+
+  int value = int.parse(stdin.readLineSync()!);
+
+  switch(value){
+    case int n when n.isEven:
+      print('$n is Even');
+      break;
+    case int n when n.isOdd:
+      print('$n is Odd');
+      break;
+    default:
+      print('Not integer');
+  }
+}
