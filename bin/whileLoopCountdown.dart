264e878a053970b438fdb286d8c16ba26c86842f
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-12 18:57:16 +0600

    Number countdown using while loop

---
+++ bin/whileLoopCountdown.dart
@@ -0,0 +1,17 @@
+void main(){
+  int i=-10;
+  while(i<0){
+    print('Number:$i');
+    i++;
+  }
+  print('End!');
+//------------------------
+
+  int countdown=5;
+
+  while(countdown>0){
+    print('Countdown:$countdown');
+    countdown--;
+  }
+  print('Launch!');
+}
