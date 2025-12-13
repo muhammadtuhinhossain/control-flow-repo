8f1e3f7349dc11c1b1b85d27d4343706e02b0cf7
MohammadTuhin1 <tuhinhossain55555@gamil.com>
2025-12-13 11:46:16 +0600

    Add for-in loop for list of collections

---
+++ bin/collections(for-in-loop).dart
@@ -0,0 +1,14 @@
+void main(){
+  //looping through a List
+  var fruits=['Apple','Banana','Lichi'];
+  print('Fruit List:');
+  for(var fruit in fruits){
+    print(fruits);
+  }
+
+  var students=['Rahim','Karim','Salam'];
+  print('\nStudents Report: ');
+  for(var student in students){
+    print('Report for $student');
+  }
+}
