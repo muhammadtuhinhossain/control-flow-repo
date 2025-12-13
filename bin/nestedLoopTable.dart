44dc3e439e62230d0f9b31b379a5d05b65947a3a
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2025-12-13 13:28:35 +0600

    Separator between table

---
+++ bin/nestedLoopTable.dart
@@ -0,0 +1,10 @@
+void main(){
+  print('Multiplication Table');
+  for(var i=1; i<=5; i++){
+    print('Multiplication Table for $i');
+    for(var j=1; j<=10; j++){
+      print('$i * $j=${i*j}');
+    }
+  }
+
+}
